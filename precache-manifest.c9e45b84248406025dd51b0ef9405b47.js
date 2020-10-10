8819b4f95cac5c6a4b58023e397d00ea757eabdc
arjun08soni <arjun08soni@gmail.com>
2020-10-10 21:22:39 +0530

    Updates

---
+++ precache-manifest.c9e45b84248406025dd51b0ef9405b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de478aae19e5f9f80d064f12de83281",
+    "url": "/urbanstop-internship/index.html"
+  },
+  {
+    "revision": "1c25be0a576c7da217c7",
+    "url": "/urbanstop-internship/static/css/main.e9d6764b.chunk.css"
+  },
+  {
+    "revision": "60f1216b1a84d548b404",
+    "url": "/urbanstop-internship/static/js/2.a2cccd85.chunk.js"
+  },
+  {
+    "revision": "09f88609baee4064210a1cec92459432",
+    "url": "/urbanstop-internship/static/js/2.a2cccd85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c25be0a576c7da217c7",
+    "url": "/urbanstop-internship/static/js/main.b85025f4.chunk.js"
+  },
+  {
+    "revision": "a65742ae6f5850bf4591",
+    "url": "/urbanstop-internship/static/js/runtime-main.9262a08f.js"
+  }
+]);
